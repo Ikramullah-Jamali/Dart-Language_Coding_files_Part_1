8042c138d86c789189d2f10a6149e40ac4f0726a
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2026-06-24 00:04:32 +0500

    Add files via upload

---
+++ Comments.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  //  This is single line comment
+
+  /*  This is multi_line comment.
+      It can be used to explain large block of code.
+  */
+
+  /// This is a documentation comment.
+  /// It is used to describe classes,functions, etc.
+
+  print("This file demonstrates Dart comments.");
+}
