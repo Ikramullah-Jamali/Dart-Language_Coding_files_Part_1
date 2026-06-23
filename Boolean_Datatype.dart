97f9e8072e67d33dde0fc7a530463e0ef9cc3ab5
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2026-06-23 23:59:49 +0500

    Add files via upload

---
+++ Boolean_Datatype.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  print("--- Boolean Datatype ---");
+
+  // Declaration and Initialization
+  bool isLoggedIn=true;
+  bool hasPermission=false;
+  print("Is Logged In: $isLoggedIn");
+  print("Has Permision $hasPermission");
+
+  // Boolean from comparision
+  int a=10;
+  int b=20;
+
+  bool result1=a<b;
+  bool result2=a>b;
+  print("a<b: $result1");
+  print("a>b: $result2");
+
+}
